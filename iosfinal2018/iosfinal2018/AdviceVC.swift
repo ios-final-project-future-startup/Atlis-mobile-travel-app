1dadd466a6a4a8e4f64abe16d81b07c4604331d7
Zachary Kimelheim <zackkimelheim@gmail.com>
2018-04-10 23:53:05 -0400

    Added view controllers

---
+++ iosfinal2018/iosfinal2018/AdviceVC.swift
@@ -0,0 +1,35 @@
+//
+//  AdviceVC.swift
+//  iosfinal2018
+//
+//  Created by Zachary Kimelheim on 4/10/18.
+//  Copyright © 2018 Zachary Kimelheim. All rights reserved.
+//
+
+import UIKit
+
+class AdviceVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
