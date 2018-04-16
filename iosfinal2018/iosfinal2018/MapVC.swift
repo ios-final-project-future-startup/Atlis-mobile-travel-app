f293bc8ee56a85c3b4f70b83c16da33120515fd1
Zachary Kimelheim <zackkimelheim@gmail.com>
2018-04-16 13:43:23 -0400

    configured contacts to appear in table view

--- iosfinal2018/iosfinal2018/MapVC.swift
+++ iosfinal2018/iosfinal2018/MapVC.swift
@@ -11,10 +11,12 @@ import UIKit
 import Firebase
 import GoogleMaps
 import GooglePlaces
+import MapKit
 
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var map: MKMapView!
     
 
     override func viewDidLoad() {
@@ -32,27 +34,27 @@ class MapVC: UIViewController {
     
     
     //temp google maps method, do not use
-    override func loadView() {
-        
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
-        
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
-        
-        //adds button on top of map
-        let button = UIButton(frame: CGRect(x: 30, y: 30, width: 50, height: 50))
-        button.backgroundColor = .blue
-        button.setTitle("Btn", for: .normal)
-        self.view.addSubview(button)
-    }
+//    override func loadView() {
+//
+//        // Create a GMSCameraPosition that tells the map to display the
+//        // coordinate -33.86,151.20 at zoom level 6.
+//        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+//        view = mapView
+//
+//        // Creates a marker in the center of the map.
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = mapView
+//
+//        //adds button on top of map
+//        let button = UIButton(frame: CGRect(x: 30, y: 30, width: 50, height: 50))
+//        button.backgroundColor = .blue
+//        button.setTitle("Btn", for: .normal)
+//        self.view.addSubview(button)
+//    }
     
     
 
