fd2eb6240bf54d337dd35c308292d127d67be791
Denisa <dv758@nyu.edu>
2018-04-22 14:15:01 -0400

    merge everything

--- iosfinal2018/iosfinal2018/View/ContactCell.swift
+++ iosfinal2018/iosfinal2018/View/ContactCell.swift
@@ -1,28 +1,26 @@
-//
-//  ContactCell.swift
-//  iosfinal2018
-//
-//  Created by Zachary Kimelheim on 4/16/18.
-//  Copyright © 2018 Zachary Kimelheim. All rights reserved.
-//
 
+//  ContactCell.swift
+//  iosfinal2018
+//
+//  Created by Zachary Kimelheim on 4/16/18.
+//  Copyright © 2018 Zachary Kimelheim. All rights reserved.
+//
 import UIKit
 
 class ContactCell: UITableViewCell {
-
+    
     @IBOutlet weak var nameLbl: UILabel!
     @IBOutlet weak var phoneNumberLbl: UILabel!
-    
-    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
-
+// Initialization code
+        
+}
+    
+    //    override func setSelected(_ selected: Bool, animated: Bool) {
+    //        super.setSelected(selected, animated: animated)
+    //
+    //        // Configure the view for the selected state
+    //    }
+    
 }
