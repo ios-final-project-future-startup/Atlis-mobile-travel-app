a3a42486128a85a6e40bd614c5f62d3369b220bd
Denisa <dv758@nyu.edu>
2018-04-23 10:37:03 -0400

    show text when searching

--- iosfinal2018/iosfinal2018/Controller/MapVC.swift
+++ iosfinal2018/iosfinal2018/Controller/MapVC.swift
@@ -15,9 +15,16 @@ import MapKit
 
 class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UISearchBarDelegate {
     
-    
     @IBOutlet weak var map: MKMapView!
     
+    
+//    var searchController:UISearchController!
+//    var localSearchRequest:MKLocalSearchRequest!
+//    var localSearch:MKLocalSearch!
+//    var localSearchResponse:MKLocalSearchResponse!
+//    var error:NSError!
+//    var pointAnnotation:MKPointAnnotation!
+//    var pinAnnotationView:MKPinAnnotationView!
     fileprivate var searchController: UISearchController!
     fileprivate var localSearchRequest: MKLocalSearchRequest!
     fileprivate var localSearch: MKLocalSearch!
@@ -30,6 +37,7 @@ class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UIS
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.definesPresentationContext = true
         
         //Configure Firebase app
         if locationManager == nil {
@@ -41,10 +49,9 @@ class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UIS
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
         
+        let searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.search, target: self, action: #selector(MapVC.searchButtonAction(_:)))
+        self.navigationItem.rightBarButtonItem = searchButton
         
-//        let searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.search, target: self, action: #selector(MapVC.searchButtonAction(_:)))
-//        self.navigationItem.rightBarButtonItem = searchButton
-
         map.delegate = self
         
         activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
@@ -64,7 +71,89 @@ class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UIS
         activityIndicator.center = self.view.center
     }
     
-   
+//    @IBAction func showSearchBar(_ sender: Any) {
+//        searchController = UISearchController(searchResultsController: nil)
+//        searchController.hidesNavigationBarDuringPresentation = false
+//        self.searchController.searchBar.delegate = self
+//        present(searchController, animated: true, completion: nil)
+//    }
+//
+//
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+//        //1
+//        searchBar.resignFirstResponder()
+//        dismiss(animated: true, completion: nil)
+//        if self.map.annotations.count != 0{
+//            annotation = self.map.annotations[0]
+//            self.map.removeAnnotation(annotation)
+//        }
+//        //2
+//        localSearchRequest = MKLocalSearchRequest()
+//        localSearchRequest.naturalLanguageQuery = searchBar.text
+//        localSearch = MKLocalSearch(request: localSearchRequest)
+//        localSearch.start { (localSearchResponse, error) -> Void in
+//
+//            if localSearchResponse == nil{
+//                let alertController = UIAlertController(title: nil, message: "Place Not Found", preferredStyle: UIAlertControllerStyle.alert)
+//                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+//                self.present(alertController, animated: true, completion: nil)
+//                return
+//            }
+//            //3
+//            self.pointAnnotation = MKPointAnnotation()
+//            self.pointAnnotation.title = searchBar.text
+//            self.pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude, longitude:     localSearchResponse!.boundingRegion.center.longitude)
+//
+//
+//            self.pinAnnotationView = MKPinAnnotationView(annotation: self.pointAnnotation, reuseIdentifier: nil)
+//            self.map.centerCoordinate = self.pointAnnotation.coordinate
+//            self.map.addAnnotation(self.pinAnnotationView.annotation!)
+//        }
+//    }
+//
+    @objc func searchButtonAction(_ button: UIBarButtonItem) {
+        if searchController == nil {
+            searchController = UISearchController(searchResultsController: nil)
+        }
+        searchController.hidesNavigationBarDuringPresentation = true
+        self.searchController.searchBar.delegate = self
+        present(searchController, animated: true, completion: nil)
+    }
+    
+    // MARK: - UISearchBarDelegate
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        dismiss(animated: true, completion: nil)
+        
+        if self.map.annotations.count != 0 {
+            annotation = self.map.annotations[0]
+            self.map.removeAnnotation(annotation)
+        }
+        
+        localSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = searchBar.text
+        localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.start { [weak self] (localSearchResponse, error) -> Void in
+            
+            if localSearchResponse == nil {
+                let alert = UIAlertView(title: nil, message: "Place not found", delegate: self, cancelButtonTitle: "Try again")
+                alert.show()
+                return
+            }
+            
+            let pointAnnotation = MKPointAnnotation()
+            pointAnnotation.title = searchBar.text
+            pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude, longitude: localSearchResponse!.boundingRegion.center.longitude)
+            
+            let pinAnnotationView = MKPinAnnotationView(annotation: pointAnnotation, reuseIdentifier: nil)
+            self!.map.centerCoordinate = pointAnnotation.coordinate
+            self!.map.addAnnotation(pinAnnotationView.annotation!)
+        }
+    }
+    
+
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         
