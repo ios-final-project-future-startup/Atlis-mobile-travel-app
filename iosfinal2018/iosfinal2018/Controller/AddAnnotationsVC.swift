473f875624cad3bf9917c7ea3a301f7d4692c612
Zachary Kimelheim <zackkimelheim@gmail.com>
2018-04-29 16:09:06 -0400

    working on add

--- iosfinal2018/iosfinal2018/Controller/AddAnnotationsVC.swift
+++ iosfinal2018/iosfinal2018/Controller/AddAnnotationsVC.swift
@@ -10,7 +10,8 @@ import UIKit
 import Firebase
 import MapKit
 import Eureka
-
+import Alamofire
+import SwiftyJSON
 
 class AddAnnotationsVC: FormViewController, MKMapViewDelegate,
 CLLocationManagerDelegate{
@@ -37,6 +38,35 @@ CLLocationManagerDelegate{
                 let formvalues = self.form.values()
                 print(formvalues)
 //               self.performSegue(withIdentifier: "MapVC", sender: nil)
+                
+                
+                let userID = self.user.uid
+                
+                let query = "\(name)&\(address)"
+                let link = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=\(query)&key=AIzaSyBiDY9xYSfMh_VKXZ9cvo4BBItW96aqqig"
+                
+                //api request for json
+//                Alamofire.request(link).responseJSON { response in
+//                    if let result = response.result.value {
+//                        let json = JSON(result);
+//
+//
+//                    }
+//                }
+                
+                Alamofire.request(link, method: .get).validate().responseJSON { response in
+                    switch response.result {
+                    case .success(let value):
+                        let json = JSON(value)
+                        let place = json["results"][0]
+                        
+                        
+                        
+                        print("JSON: \(json)")
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
         }
     }
     
