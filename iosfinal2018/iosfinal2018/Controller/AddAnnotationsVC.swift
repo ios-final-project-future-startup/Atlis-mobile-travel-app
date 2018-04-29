0148bd21d78ef13cbb8b054362ea8d836b6fb4c9
Zachary Kimelheim <zackkimelheim@gmail.com>
2018-04-29 16:36:28 -0400

    alamofire

--- iosfinal2018/iosfinal2018/Controller/AddAnnotationsVC.swift
+++ iosfinal2018/iosfinal2018/Controller/AddAnnotationsVC.swift
@@ -48,28 +48,32 @@ CLLocationManagerDelegate{
 //               self.performSegue(withIdentifier: "MapVC", sender: nil)
                 
                 
-                let userID = self.user.uid
+               
                 
-                let query = "\(name)&\(address)"
+                //prep data for query to include "+" instead of " "
+                let queryName  = self.name.replacingOccurrences(of: " ", with: "+", options: .literal, range: nil)
+                let queryAddress = self.address.replacingOccurrences(of: " ", with: "+", options: .literal, range: nil)
+                
+                let query = "\(queryName)&\(queryAddress)"
                 let link = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=\(query)&key=AIzaSyBiDY9xYSfMh_VKXZ9cvo4BBItW96aqqig"
                 
-                //api request for json
-//                Alamofire.request(link).responseJSON { response in
-//                    if let result = response.result.value {
-//                        let json = JSON(result);
-//
-//
-//                    }
-//                }
-                
+                //get JSON and save it in database properly
                 Alamofire.request(link, method: .get).validate().responseJSON { response in
                     switch response.result {
                     case .success(let value):
                         let json = JSON(value)
                         let place = json["results"][0]
                         
-                        
-                        
+                        //saving to users -> userid -> saved_recommendations -> *placeID -> *
+                        let ref = Database.database().reference().child("users").child(self.user.uid).child("savedRecommendations").child(place["placeID"].string!)
+                        ref.setValue(["name":place["name"].string!])
+                        ref.setValue(["address":place["formatted_address"].string!])
+                        ref.setValue(["icon":place["icon"].string!])
+                        ref.setValue(["price_level":place["price_level"].string!])
+                        ref.setValue(["lat":place["geometry"]["location"]["lat"].string!])
+                        ref.setValue(["lon":place["geometry"]["location"]["lng"].string!])
+                        ref.setValue(["rating":place["rating"].string!])
+                        ref.setValue(["from":self.titleBox])
                         print("JSON: \(json)")
                     case .failure(let error):
                         print(error)
