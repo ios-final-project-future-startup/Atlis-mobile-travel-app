785b2fa92e6f132ba7803a8d37c77ab158ffa51b
Denisa <dv758@nyu.edu>
2018-04-29 15:32:32 -0400

    Add new place page

--- iosfinal2018/iosfinal2018/Controller/AddAnnotationsVC.swift
+++ iosfinal2018/iosfinal2018/Controller/AddAnnotationsVC.swift
@@ -11,19 +11,33 @@ import Firebase
 import MapKit
 import Eureka
 
+
 class AddAnnotationsVC: FormViewController, MKMapViewDelegate,
 CLLocationManagerDelegate{
     @IBOutlet weak var addLabel: UILabel!
+     var user: User!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        form +++ Section()
+        form +++ Section("Add a Place")
+            
             <<< TextRow(){ row in
                 row.title = "Name of Place"
             }
             <<< TextRow() { row in
                 row.title = "Address/ City"
             }
+            <<< TextRow() { row in
+                row.title = "Recommended by"
+        }
+        <<< ButtonRow() {
+            $0.title = "Add"
+            }
+            .onCellSelection {  cell, row in
+                let formvalues = self.form.values()
+                print(formvalues)
+//               self.performSegue(withIdentifier: "MapVC", sender: nil)
+        }
     }
     
 }
