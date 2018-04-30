bd89d8aefccc0c41d6df6b0cdbd8f0c9307fb460
Arjun Madgavkar <am5457@stern.nyu.edu>
2018-04-30 15:11:24 -0400

    add swift multiselect ---could use as master

---
+++ iosfinal2018/iosfinal2018/Controller/SelectContactsVC.swift
@@ -0,0 +1,25 @@
+//
+//  SelectContactsVC.swift
+//  iosfinal2018
+//
+//  Created by Arjun Madgavkar on 4/30/18.
+//  Copyright © 2018 Zachary Kimelheim. All rights reserved.
+//
+
+import UIKit
+import SwiftMultiSelect
+
+class SelectContactsVC: UIViewController {
+    
+    // MARK: Init
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: UI
+    
+}
